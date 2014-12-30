12635ebf6b2e3744324ec216d06282f9d2ff8ee1
Bob Lail <bob.lail@cph.org>
2014-12-30 14:55:03 -0600

    [refactor] Added support for deprecated job numbers (1h)

--- lib/star/job.rb
+++ lib/star/job.rb
@@ -1,7 +1,7 @@
-require "multi_json"
+require "yaml"
 
 class Star
-  class Job < Struct.new(:project, :component, :job_number)
+  class Job < Struct.new(:project, :component, :job_number, :deprecated)
     
     PROJECT_ALIASES = {
       "360" => "members",
@@ -13,7 +13,8 @@ class Star
     
     COMPONENT_ALIASES = {
       "incubation" =>   "planning",
-      "enhancement" =>  "improvement",
+      "enhancement" =>  "feature",
+      "improvement" =>  "feature",
       "bugfix" =>       "fix",
       "testfix" =>      "chore",
       "cifix" =>        "chore",
@@ -24,46 +25,42 @@ class Star
     }.freeze
     
     
+    
     def self.projects
       @projects ||= entries.map(&:project).uniq
     end
     
     def self.legal_projects
-      projects + PROJECT_ALIASES.keys
-    end
-    
-    def self.components
-      @components ||= entries.map(&:component).uniq
-    end
-    
-    def self.legal_components
-      components + COMPONENT_ALIASES.keys
+      entries.reject(&:deprecated).map(&:project).uniq + PROJECT_ALIASES.keys
     end
     
     def self.valid_project?(project)
-      projects.member? normalize_project(project)
+      legal_projects.member? normalize_project(project)
     end
     
     def self.normalize_project(project)
       PROJECT_ALIASES.fetch(project, project)
     end
     
-    def self.entries_for_project(project)
-      project = normalize_project(project)
-      entries.select { |entry| entry.project == project }
-    end
+    
     
     def self.components_for_project(project)
       entries_for_project(project).map(&:component)
     end
     
+    def self.legal_components_for_project(project)
+      entries_for_project(project).reject(&:deprecated).map(&:component) + COMPONENT_ALIASES.keys
+    end
+    
     def self.valid_component_for_project?(component, project)
-      components_for_project(project).member? normalize_component(component)
+      legal_components_for_project(project).member? normalize_component(component)
     end
     
     def self.normalize_component(component)
       COMPONENT_ALIASES.fetch(component, component)
     end
+    
+    
     
     def self.entry_for_project_and_component(project, component)
       entries_for_project = entries_for_project(project)
@@ -79,6 +76,8 @@ class Star
       [entry.project, entry.component] if entry
     end
     
+    
+    
   private
     
     def self.entries
@@ -86,28 +85,22 @@ class Star
     end
     
     def self.read_entries!
-      data = MultiJson.load File.read(JOB_NUMBERS_DATA_PATH)
+      data = YAML.load File.read(JOB_NUMBERS_DATA_PATH)
       
-      # Ensure that we have only one job number per 
-      # project-component combination.
-      entries_by_project_and_component = {}.tap do |map|
-        data["projects"].each do |project_slug, job_number|
-          data["components"].each do |component_slug, component_number|
-            map[[project_slug, component_slug]] = "#{job_number}-#{component_number}"
-          end
-        end
-        
-        data.fetch("job_numbers", {}).each do |project_slug, job_number|
-          map[project_slug.split("-")] = job_number
-        end
-      end
-      
-      entries_by_project_and_component.map do |(project, component), job_number|
-        new(project, component, job_number)
-      end
+      data["jobs"].flat_map { |project_number, job|
+        job["components"].map { |component_number, component_slug| new(
+          job["name"],
+          component_slug,
+          "#{project_number}-#{component_number}",
+          !!job["deprecated"]) } }
     end
     
-    JOB_NUMBERS_DATA_PATH = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "data", "job_numbers.json")).freeze
+    def self.entries_for_project(project)
+      project = normalize_project(project)
+      entries.select { |entry| entry.project == project }
+    end
+    
+    JOB_NUMBERS_DATA_PATH = File.expand_path("../../../data/job_numbers.yml", __FILE__).freeze
     
   end
 end
